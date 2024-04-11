fa312944f3d4dbf099c8b9e8f5dfc894db9805ce
hthau1909 <67138174+hthau1909@users.noreply.github.com>
2024-04-11 14:50:33 +0700

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+  var mixer = mixitup('.portF');
+  var nav = document.querySelector('nav');
+  window.addEventListener('scroll', function () {
+      if (window.pageYOffset > 100) {
+        nav.classList.add('navbar-reduce', 'shadow');
+      } else {
+        nav.classList.remove('navbar-reduce', 'shadow');
+      }
+  });
+
+  new TypeIt(".element", {
+      speed: 50,
+      loop: true,
+  }).type("Salesforce Developer,", { delay: 300 })
+  .delete(30)
+  .type("UI/UX Designer,", { delay: 300 })
+  .delete(50)
+  .type("Web developer.", { delay: 300 })
+  .go()
+
+  //MixItUp Portfolio
+  var mixer = mixitup('.portfolio-full');
+  var mixer = mixitup('.portF');
+  var mixer = mixitup('.portF', {
+    selectors: {
+      target: '.blog-item'
+    },
+    animation: {
+      duration: 100
+    }
+  });
+  // Portfolio Gallery
+  $('.portfolio-item').magnificPopup({
+      type: 'image',
+      gallery: {
+        enabled: true
+      }
+  });
+});
+
